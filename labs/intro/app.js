be042c2a7d95fe49ae14f20ced66f4568f59d184
katietrbl <katiestinson03@gmail.com>
2022-09-01 13:58:23 -0400

    hello

--- labs/intro/app.js
+++ labs/intro/app.js
@@ -1,1 +1,14 @@
 document.write("Hello World");
+console.log
+let size = 10;
+
+function setup(){
+    createCanvas(800,600);
+    console.log(size);
+}
+
+function draw(){
+    background(0);
+    size = size + 1;
+    circle(mousex, mouseY, 30)
+}
