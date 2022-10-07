0cba6f55ccae368c2f6bc6dbd7878dd937f7463e
katietrbl <katiestinson03@gmail.com>
2022-10-06 23:28:43 -0400

    Update loop.js

--- Week7/JS/loop.js
+++ Week7/JS/loop.js
@@ -1,2 +1,4 @@
-/*Write a loop that displays in a div the numbers 1000, 2000, 3000, 4000, 5000. There should only be one line of code inside of your loop.
+/*Write a loop that displays in a div the numbers 1000, 2000, 3000, 4000, 5000. 
+There should only be one line of code inside of your loop.
 */
+
