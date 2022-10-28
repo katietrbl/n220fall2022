c86c203441d97f99b967ca63b2d2632be2087f1d
katietrbl <katiestinson03@gmail.com>
2022-10-27 23:44:35 -0400

    Week9

--- Week9/JS/apps1.js
+++ Week9/JS/apps1.js
@@ -1,4 +1,16 @@
+
 /* Create an page with an input, and a button. 
 When the button is clicked, output the phrase 
 "Hello {Name}" to the developer console, with {Name} 
 being the value the user put into the input field. */
+
+
+//begin with setting the div of hello. 
+let speakEl = document.getElementById("hello");
+
+console.log(speakEl);
+
+function sayHello(){
+    speakEl.push(document.getElementById("userinput").value); 
+    speakEl.innerHTML = "Hello " + ("userinput") 
+}
--- Week9/JS/apps2.js
+++ Week9/JS/apps2.js
@@ -4,3 +4,25 @@ When the button is clicked (assuming the value typed into the input
     to be paid. Output both to the console, formatted as such:
 
 "Tip: $X. Total: $X"  */
+
+var myBtn = document.getElementById('my-button');
+var tipElem = document.getElementById("tip");
+var totalElem = document.getElementById("total");
+
+//attach click event listener
+myBtn.addEventListener('click', calc);
+
+//the event object is passed to this automatically - I'm assigning it to "e"
+function calc(e) {
+  //prevent the default action - form submit / page refresh
+  e.preventDefault();
+  var input = document.getElementById('bill');
+  //convert string value to Number
+  var bill = parseFloat(input.value);
+  var tip = bill * 0.15;
+  var total = bill + tip;
+
+  //textContent is better than innerHTML for setting text
+  tipElem.textContent = "$"+(tip).toFixed(2);
+  totalElem.textContent = "$"+(total).toFixed(2);  
+}
